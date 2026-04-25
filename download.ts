8734d48e25e35a832b9b94487982e41a4e5e0c84
AI-technician <no1prawess@gmail.com>
2026-04-25 19:14:44 +0900

    feat: Update logistics data URL
    Updates the URL for downloading the logistics data to fetch the latest version from the repository.

--- download.ts
+++ download.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import https from 'https';
 
-const url = 'https://raw.githubusercontent.com/AI-technician/ware2/477c2575e1749417fd119e74630cc9e3635e4df8/public/logistics.xlsx';
+const url = 'https://raw.githubusercontent.com/AI-technician/ware2/6928bd4dcc61a60e72c074fbf2e544ea1b278249/public/logistics.xlsx';
 
 https.get(url, (res) => {
   const path = 'public/logistics.xlsx';
